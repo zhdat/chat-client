ef20c6d7af647bf4ca10742e34257cc2525a6dae
Lu Blanvillain <leoblanvillain1@gmail.com>
2020-03-17 20:08:07 +0100

    some basic input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entrez votre nom d'utilisateur: ");
+		String userName = scan.nextLine();
+		
+		System.out.println("Entrez votre mot de passe: ");
+		String password = scan.nextLine();
 		
 	}
 }
